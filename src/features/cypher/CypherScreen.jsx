85d98ea6c9446ae3c250fd59dcc3292b9ecded39
André Masson <amwebexpert@gmail.com>
2022-12-27 16:37:37 -0600

    feat: encrypt/decrypt data using crypto-es

--- src/features/cypher/CypherScreen.jsx
+++ src/features/cypher/CypherScreen.jsx
@@ -2,32 +2,60 @@ import * as React from "react";
 import { View, StyleSheet, ToastAndroid } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import { AppLayout } from "~/components/layout/AppLayout";
+import CryptoES from "crypto-es";
 
 export const CypherScreen = () => {
-  const [text, setText] = React.useState("Here the text to encrypt");
+  const [text, setText] = React.useState("Data: ➡ Here the original data in clear text.");
+  const [result, setResult] = React.useState("");
 
   const encryptData = () => {
-    ToastAndroid.show(text, ToastAndroid.SHORT);
-  }
+    const result = CryptoES.AES.encrypt(text, "my-passphrase");
+    setResult(result.toString());
+  };
+
+  const decryptData = () => {
+    const result = CryptoES.AES.decrypt(text, "my-passphrase");
+    setResult(result.toString(CryptoES.enc.Utf8));
+  };
+
+  const switchResult = () => {
+    setText(result);
+    setResult("");
+  };
 
   return (
     <AppLayout title="Cypher screen">
       <View style={styles.root}>
         <TextInput
-          label="Text to encrypt"
+          label="Data"
           placeholder="Type some text here"
           mode="outlined"
           value={text}
           onChangeText={(text) => setText(text)}
         />
+        <TextInput
+          label="Result"
+          multiline={true}
+          numberOfLines={5}
+          placeholder="Type some text here"
+          mode="outlined"
+          value={result}
+        />
       </View>
 
-      <Button
-          mode="contained"
-          onPress={encryptData}
-        >
+      <View style={styles.actions}>
+        <Button mode="contained" onPress={encryptData}>
           Encrypt
         </Button>
+
+        <Button mode="contained" onPress={decryptData}>
+          Decrypt
+        </Button>
+
+        <Button mode="contained" onPress={switchResult}>
+          Switch result
+        </Button>
+      </View>
     </AppLayout>
   );
 };
@@ -35,5 +63,9 @@ export const CypherScreen = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 });
