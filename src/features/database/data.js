62321ffb03bde727b8ce54bda5d517197c93bb08
André Masson <amwebexpert@gmail.com>
2022-12-29 11:52:56 -0600

    feat: archive FILE table basic example

---
+++ src/features/database/data.js
@@ -0,0 +1,7 @@
+export const LONG_TEXT = `React Native history
+
+In 2012 Mark Zuckerberg commented, "The biggest mistake we made as a company was betting too much on HTML as opposed to native".[12][13] Using HTML5 for Facebook's mobile version resulted in an unstable application that retrieved data slowly.[14] He promised Facebook would soon deliver a better mobile experience.
+
+Inside Facebook, Jordan Walke found a way to generate UI elements for iOS from a background JavaScript thread, which became the basis for the React web framework. They decided to organize an internal Hackathon to perfect this prototype in order to be able to build native apps with this technology.[15]
+
+In 2015, after months of development, Facebook released the first version for the React JavaScript Configuration. During a technical talk,[16] Christopher Chedeau explained that Facebook was already using React Native in production for their Group App and their Ads Manager App.[17]`;
