aeeee444889cf0e755936fac5bc46e63f2d7957a
amwebexpert <amwebexpert@gmail.com>
2023-01-08 16:28:39 -0600

    renamed topLeft and bottomRight

--- src/features/picture-region-selector/RegionSelector.js
+++ src/features/picture-region-selector/RegionSelector.js
@@ -5,26 +5,19 @@ import { MovableCircleHandle } from "./MovableCircleHandle";
 import { INITIAL_PADDING, CIRCLE_SIZE } from "./constants";
 
 export const RegionSelector = ({ imageLayout }) => {
-  const handleTopLeft = useSharedValue({
+  const topLeft = useSharedValue({
     x: INITIAL_PADDING,
     y: INITIAL_PADDING,
   });
-  const handleBottomRight = useSharedValue({
+  const bottomRight = useSharedValue({
     x: imageLayout.width - INITIAL_PADDING - CIRCLE_SIZE,
     y: imageLayout.height - INITIAL_PADDING - CIRCLE_SIZE,
   });
 
   return (
     <>
-      <MovableCircleHandle
-        imageLayout={imageLayout}
-        position={handleTopLeft}
-      />
-
-      <MovableCircleHandle
-        imageLayout={imageLayout}
-        position={handleBottomRight}
-      />
+      <MovableCircleHandle imageLayout={imageLayout} position={topLeft} />
+      <MovableCircleHandle imageLayout={imageLayout} position={bottomRight} />
     </>
   );
 };
