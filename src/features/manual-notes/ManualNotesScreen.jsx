60f1fe8d323e3fdb4a50a2b67d84b4c82757301e
amwebexpert <amwebexpert@gmail.com>
2023-03-05 15:30:07 -0600

    feat: export canvas as jpeg into media library

--- src/features/manual-notes/ManualNotesScreen.jsx
+++ src/features/manual-notes/ManualNotesScreen.jsx
@@ -1,16 +1,24 @@
+import { useRef } from "react";
 import { StyleSheet, View } from "react-native";
 import { Button, IconButton } from "react-native-paper";
 import { useSharedValue } from "react-native-reanimated";
+import { captureRef } from "react-native-view-shot";
+import * as MediaLibrary from "expo-media-library";
 
 import { AppLayout } from "~/components/layout/AppLayout";
+import { useSnackbar } from "~/components/snack-bar/useSnackbar";
 import * as svgUtils from "./svg-utils";
 import { useElements } from "./hooks/useElements";
 import { DEFAULT_NOTES_URI } from "./constants";
 import { usePenStyle } from "./hooks/usePenStyle";
-import { SvgViewer } from "./SvgViewer";
+import SvgViewer from "./SvgViewer";
 import { PathGestureDrawer } from "./PathGestureDrawer";
+import { DEFAULT_IMAGE_OPTIONS } from "./snapshot-utils";
 
 export const ManualNotesScreen = () => {
+  const imageRef = useRef();
+  const showSnackbarMessage = useSnackbar();
+
   const { elements, setElements, hasElements, addElement, removeLastElement, clearElements } = useElements();
   const { penStyle } = usePenStyle();
   const gesturePoints = useSharedValue([]);
@@ -41,6 +49,18 @@ export const ManualNotesScreen = () => {
   };
   const exportAsSvg = () => svgUtils.exportAsSvg({ elements, fileUri: DEFAULT_NOTES_URI });
 
+  const saveAsBitmap = async () => {
+    try {
+      const localUri = await captureRef(imageRef, DEFAULT_IMAGE_OPTIONS);
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        showSnackbarMessage("Saved into media library!");
+      }
+    } catch (e) {
+      showSnackbarMessage(e.message);
+    }
+  };
+
   return (
     <AppLayout title="Hand written notes">
       <PathGestureDrawer
@@ -50,7 +70,7 @@ export const ManualNotesScreen = () => {
         addPath={addPath}
       />
 
-      <SvgViewer elements={elements} style={styles.svgElements} />
+      <SvgViewer elements={elements} style={styles.svgElements} ref={imageRef} collapsable={false} />
 
       <View style={styles.actions}>
         <IconButton
@@ -61,6 +81,13 @@ export const ManualNotesScreen = () => {
           style={styles.iconButton}
         />
         <IconButton mode="outlined" onPress={undo} icon="undo" disabled={!hasElements} style={styles.iconButton} />
+        <IconButton
+          mode="outlined"
+          onPress={saveAsBitmap}
+          icon="camera"
+          disabled={!hasElements}
+          style={styles.iconButton}
+        />
 
         <Button mode="outlined" onPress={importSvg} icon="file-import">
           Import
--- src/features/manual-notes/SvgViewer.jsx
+++ src/features/manual-notes/SvgViewer.jsx
@@ -1,13 +1,13 @@
+import React from "react";
 import Svg, { Path } from "react-native-svg";
 
 const EmptyView = () => null;
 
-const PathView = ({ d, strokeColor, strokeWidth }) => (
-  <Path d={d} stroke={strokeColor} strokeWidth={strokeWidth} />
-);
+const PathView = ({ d, strokeColor, strokeWidth }) => <Path d={d} stroke={strokeColor} strokeWidth={strokeWidth} />;
 
 const ELEMENT_VIEWERS = new Map([
   ["path", PathView],
+  //...other elements supported by react-native-svg will be supported (not yet implemented)
   ["circle", EmptyView],
   ["rect", EmptyView],
   ["ellipse", EmptyView],
@@ -15,14 +15,19 @@ const ELEMENT_VIEWERS = new Map([
   ["polyline", EmptyView],
   ["polygon", EmptyView],
   ["text", EmptyView],
-  //...other elements supported by react-native-svg
 ]);
 
-export const SvgViewer = ({ elements, ...others }) => (
-  <Svg height="100%" width="100%" {...others}>
-    {elements.map((item, i) => {
-      const ElementViewer = ELEMENT_VIEWERS.get(item.type);
-      return <ElementViewer key={i} {...item} />;
-    })}
-  </Svg>
-);
+const SvgViewer = React.forwardRef((props, ref) => {
+  const { elements, ...rest } = props;
+
+  return (
+    <Svg height="100%" width="100%" {...rest} ref={ref}>
+      {elements.map((item, i) => {
+        const ElementViewer = ELEMENT_VIEWERS.get(item.type);
+        return <ElementViewer key={i} {...item} />;
+      })}
+    </Svg>
+  );
+});
+
+export default SvgViewer;
